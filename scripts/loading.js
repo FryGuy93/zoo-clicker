84b0754b370c0128f71aab99e32d7b65b7dc12ee
Ian Harcourt-Smith <ianearnest@hotmail.co.uk>
2024-04-06 05:11:16 +0200

    HTML seperated into components

---
+++ scripts/loading.js
@@ -0,0 +1,8 @@
+// On page load - insert HTML
+$(function() { $("#netlify").load("../components/netlify.html"); });
+$(function() { $("#glossary").load("../components/glossary.html"); });
+$(function() { $("#toasts").load("../components/toasts.html"); });
+$(function() { $("#topbarload").load("../components/topbar.html"); });
+$(function() { $("#banner").load("../components/banner.html"); });
+$(function() { $("#unlocks").load("../components/unlocks.html"); });
+$(function() { $("#exhibits").load("../components/exhibits.html"); });
