c2b0cb63b0f8718b03c670c44cd81307e6938430
Ian Harcourt-Smith <ianearnest@hotmail.co.uk>
2024-05-15 01:40:09 +0200

    toast close buttons fix

--- scripts/toast.js
+++ scripts/toast.js
@@ -27,25 +27,29 @@ function openToast_Error(MyInfo) {
 
 }
 
-// Get all elements with class="closebtn"
-var close = document.getElementsByClassName("closebtn");
-var i;
+closeBtns();
+function closeBtns() {
+	// Get all elements with class="closebtn"
+	var close = document.getElementsByClassName("closebtn");
+	var i;
 
-// Loop through all close buttons
-for (i = 0; i < close.length; i++) {
-  // When someone clicks on a close button
-  close[i].onclick = function(){
+	// Loop through all close buttons
+	for (i = 0; i < close.length; i++) {
+		// When someone clicks on a close button
+		close[i].onclick = function(){
 
-    // Get the parent of <span class="closebtn"> (<div class="alert">)
-    var div = this.parentElement;
+			// Get the parent of <span class="closebtn"> (<div class="alert">)
+			var div = this.parentElement;
 
-    // Set the opacity of div to 0 (transparent)
-    div.style.opacity = "0";
+			// Set the opacity of div to 0 (transparent)
+			div.style.opacity = "0";
 
-    // Hide the div after 600ms (the same amount of milliseconds it takes to fade out)
-    setTimeout(function(){ div.style.display = "none"; }, 600);
-  }
+			// Hide the div after 600ms (the same amount of milliseconds it takes to fade out)
+			setTimeout(function(){ div.style.display = "none"; }, 600);
+		}
+	}
 }
+
 
 // TESTING
 // When the user clicks on div, open the popup
