f5c0d794ac056b5c0ce47b09df86f48fd07ba057
Ian Harcourt-Smith <ianearnest@hotmail.co.uk>
2024-05-15 01:40:58 +0200

    Loading simplified, error checking - toast on error

--- scripts/loading.js
+++ scripts/loading.js
@@ -6,63 +6,37 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree. 
 */
 
-// Loop to wait until finished loading... or timeout
-var waitingForPageLoaded_IntervalId = window.setInterval(waitingForPageLoaded, 100);
-function waitingForPageLoaded() {
-    console.log("Page not loaded yet...");
-
-    if(isPageFinishedLoading == true){
-        console.log("Finished loading page confirmed...");
-        window.clearInterval(waitingForPageLoaded_IntervalId);
-        startScript();
-    }
-}
-
 // On page load - insert HTML
 $(function() { 
-    $("#div1").load("demo_test.txt", function (response, status, xhr){
-        loadingProcess(response, status, xhr)
-    });
+    console.log("Loading page...");
     if (isDebugMode) {
-		$(function() { 
-			$("#debugbardiv").load("../components/debugbar.html", function (response, status, xhr){
-				loadingProcess(response, status, xhr)
-			});
-		});
+        loadElement("debugbar");
     }
-    //$("#div1").load("demo_test.txt", loadingProcess(response, status, xhr));
-    $("#netlify").load("../components/netlify.html", function (response, status, xhr){
-        loadingProcess(response, status, xhr)
-    });
-    $("#glossary").load("../components/glossary.html", function (response, status, xhr){
-        loadingProcess(response, status, xhr)
-    });
-    $("#stats").load("../components/stats.html", function (response, status, xhr){
-        loadingProcess(response, status, xhr)
-    });
-    $("#achievements").load("../components/achievements.html", function (response, status, xhr){
-        loadingProcess(response, status, xhr)
-    });
-    $("#toasts").load("../components/toasts.html", function (response, status, xhr){
-        loadingProcess(response, status, xhr)
-    });
-    $("#topbarload").load("../components/topbar.html", function (response, status, xhr){
-        loadingProcess(response, status, xhr)
-    });
-    $("#banner").load("../components/banner.html", function (response, status, xhr){
-        loadingProcess(response, status, xhr)
-    });
-    $("#unlocks").load("../components/unlocks.html", function (response, status, xhr){
-        loadingProcess(response, status, xhr)
-    });
-    $("#exhibits").load("../components/exhibits.html", function (response, status, xhr){
-        loadingProcess(response, status, xhr, true)
-    });
+    loadElement("netlify");
+    loadElement("ui");
+    loadElement("toasts");
+    loadElement("topbar");
+    loadElement("stats");
+    loadElement("achievements");
+    loadElement("banner");
+    loadElement("unlocks");
+    loadElement("exhibits");
+    loadElement("glossary");
+    
+    loadElement("footer", true);
 });
+// elementName = 
+//  name of element on index page = "<div id="banner">" 
+//  and name of file = "banner.html"
+function loadElement(elementName, isFinished){
+    $("#"+elementName).load("../components/"+elementName+".html", function (response, status, xhr){
+        loadingProcess(response, status, xhr, isFinished)
+    });
+}
 
 function loadingProcess(responseTxt, statusTxt, xhr, isFinished){
     if(statusTxt == "success"){
-        console.log("External content loaded successfully! - Content: " + responseTxt.substring(0, 20) + "...");
+        console.log("\tSuccess - External content loaded! - Content: " + responseTxt.substring(0, 20) + "...");
     }
     if(statusTxt == "error"){
         console.log("Error: " + xhr.status + ": " + xhr.statusText);
@@ -71,12 +45,35 @@ function loadingProcess(responseTxt, statusTxt, xhr, isFinished){
 
     // only on last element...
     if(isFinished == true){
-        console.log("Finished loading page...");
         isPageFinishedLoading = true;
     }
 }
 
 // Say Hi
+/*
 setTimeout(function(){
     openToast("Welcome!");
 }, 1000); 
+*/
+
+
+// Loop to wait until finished loading... or timeout
+var waitingForPageLoaded_IntervalId = window.setInterval(waitingForPageLoaded, 100);
+function waitingForPageLoaded() {
+    if(isPageFinishedLoading == true){
+        console.log("Page loaded");
+        window.clearInterval(waitingForPageLoaded_IntervalId);
+        startScript();
+    }
+    if(isPageFinishedLoading == false){
+        console.log("Page not loaded yet...");
+    }
+}
+
+
+// Error handler
+window.onerror = function myErrorHandler(errorMsg, url, lineNumber) {
+    openToast("Error occured: " + errorMsg);
+    //alert("Error occured: " + errorMsg);
+    return false;
+}
